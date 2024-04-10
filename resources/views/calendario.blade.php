7b1bb650920e1f44d2fb3782b7332fe76dfa0221
rla28-ua <rla28@alu.ua.es>
2024-04-10 20:38:16 +0200

    Vista 'Calendario'

--- resources/views/calendario.blade.php
+++ resources/views/calendario.blade.php
@@ -1,13 +1,52 @@
-<!-- resources/views/jugadores.blade.php -->
 @extends('layout')
 
-@section('title', 'La Liga')
+@section('title', 'Calendario de Partidos')
 
 @section('content')
-    <h2>Calendario 23-24</h2>
+    <h1 style="text-align: center; margin-top: 10px;">Calendario de Partidos</h1>
 
-    <p>¡La vista de Calendario funciona correctamente!
-        Estás en la vista de calendario!!!!
-    </
-    <!-- Aquí puedes agregar el contenido relacionado con los jugadores -->
+    <div class="jornada-navegacion">
+    <a href="{{ route('calendario', ['jornada' => $jornada_actual - 1]) }}" class="jornada-anterior">Jornada Anterior</a>
+        <select onchange="location = this.value;" class="jornada-actual">
+            @foreach($jornadas as $jornada)
+                <option value="{{ route('calendario', ['jornada' => $jornada]) }}" @if($jornada == $jornada_actual) selected @endif>
+                    Jornada {{ $jornada }}
+                </option>
+            @endforeach
+        </select>
+        <a href="{{ route('calendario', ['jornada' => $jornada_actual + 1]) }}" class="jornada-siguiente">Jornada Siguiente</a>
+    </div>
+
+    <div class="tabla-calendario">
+        <div class="fila-partidos-head">
+            <div class="columna-fecha">
+                Fecha
+            </div>
+            <div class="columna-hora">
+                Hora
+            </div>
+            <div class="columna-resultado">
+                Partido
+            </div>
+        </div>
+
+        @foreach($partidos as $index => $partido)
+            @php
+                $claseFila = $index % 2 == 0 ? 'fila-par' : 'fila-impar';
+            @endphp
+            <div class="fila-partido {{ $claseFila }}">
+                <div class="columna-fecha">
+                    {{ $partido->fecha }}
+                </div>
+                <div class="columna-hora">
+                    {{ $partido->hora }}
+                </div>
+                <div class="columna-resultado">
+                    <strong>{{ $partido->equipoLocal->nombre }}</strong>
+                    <span class="resultado">{{ $partido->resultado }}</span>
+                    <strong>{{ $partido->equipoVisitante->nombre }}</strong>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
