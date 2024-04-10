c664fed6236a60141937244a82c8265daa9b2072
rla28-ua <rla28@alu.ua.es>
2024-04-10 20:33:37 +0200

    Controlador Calendario

--- app/Http/Controllers/CalendarioController.php
+++ app/Http/Controllers/CalendarioController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Partido;
 
 class CalendarioController extends Controller
 {
-    public function index()
+    public function index($jornada = 1)
     {
-        return view('calendario');
+        $totalJornadas = 38;
+        $jornadas = range(1, $totalJornadas);
+
+        if ($jornada < 1) {
+            $jornada = $totalJornadas; // Ir a la última jornada si se va más allá de la primera
+        } elseif ($jornada > $totalJornadas) {
+            $jornada = 1; // Ir a la primera jornada si se va más allá de la última
+        }
+
+        $jornada_actual = $jornada;
+
+        // Obtener los partidos de la jornada seleccionada con 
+        // relaciones de equipo cargadas
+        $partidos = Partido::where('jornada', $jornada)
+                           ->with('equipoLocal', 'equipoVisitante')
+                           ->get();
+
+        return view('calendario', compact('partidos', 'jornada_actual', 'jornadas'));
     }
 }
