3ce0dea20595341b525a8a478d31622431860879
psm97-ua <psm97@alu.ua.es>
2024-04-12 12:38:35 +0200

    corrección rutas a login y register

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,6 @@ Route::get('/favoritos', [FavoritosController::class, 'index'])->name('favoritos
 
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
-
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
