fdd379f8d647d76d9d4f1f524cc469ca60af741c
santinogales42 <santinogales42@gmail.com>
2024-04-12 18:19:55 +0200

    base de CRUD Equipo

---
+++ storage/framework/views/ffeebc256180d7d000cf4f4d946e94d1ff26e215.php
@@ -0,0 +1,14 @@
+<!-- resources/views/jugadores.blade.php -->
+
+
+<?php $__env->startSection('title', 'La Liga'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <h2>Noticias</h2>
+
+    <p>¡La vista de Noticiias funciona correctamente!
+        Estás en la vista de noticias!!!!
+    </
+    <!-- Aquí puedes agregar el contenido relacionado con los jugadores -->
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/usuario/Escritorio/DSS/G02-06-DSS/resources/views/noticias.blade.php ENDPATH**/ ?>
