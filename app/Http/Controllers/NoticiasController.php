04039862db695443b42f6969f2d6a972635858a3
Ruben <rcm144@alu.ua.es>
2024-04-16 14:23:46 +0200

    Vista noticias muestra noticias de la bd, pero sin formato css bonito

--- app/Http/Controllers/NoticiasController.php
+++ app/Http/Controllers/NoticiasController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Noticia;
+use App\Models\Equipo;
 use Illuminate\Http\Request;
 
 class NoticiasController extends Controller
@@ -9,5 +11,25 @@ class NoticiasController extends Controller
     public function index()
     {
         return view('noticias');
+    }
+
+    public function getDatos($id)
+    {
+        $noticia = Noticia::findOrFail($id);
+        return response()->json($noticia);
+    }
+    public function getEquipoName($id)
+    {
+        // Busca el equipo por su ID
+        $equipo = Equipo::find($id);
+
+        // Verifica si se encontró el equipo
+        if ($equipo) {
+            // Devuelve el nombre del equipo en formato JSON
+            return response()->json(['nombre' => $equipo->nombre]);
+        } else {
+            // Devuelve un mensaje de error si el equipo no se encuentra
+            return response()->json(['error' => 'Equipo no encontrado'], 404);
+        }
     }
 }
--- resources/views/noticias.blade.php
+++ resources/views/noticias.blade.php
@@ -1,51 +1,84 @@
-<!-- resources/views/jugadores.blade.php -->
+
 @extends('layout')
 
-@section('title', 'La Liga')
-
 @section('content')
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Noticias - Liga de Fútbol</title>
-    <link rel="stylesheet" href="{asset('css/app.css')}"> <!-- Asegúrate de que el enlace al archivo CSS sea correcto -->
-</head>
-<body>
-    <div class="contenedor-noticias">
-        <div class="container">
-            <h1>Noticias</h1>
-            <div class="noticia">
-                <h2><a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.marca.com/futbol/mallorca/2024/04/10/66163cc546163f16608b4581.html&ved=2ahUKEwib0cHIlLeFAxWaV6QEHa5iCBkQxfQBegQIABAE&usg=AOvVaw2SmO6ueSUAXoA0jAUbuZou">El Mallorca activa el modo liga: en busca de una permanencia tranquila</a></h2>
-                <img src="https://s1.elespanol.com/2023/05/25/deportes/futbol/766434171_233445124_1706x960.jpg">
-                <p class="descripcion">Al equipo de Aguirre le vienen cuatro encuentros complicados que determinarán si conseguirá una permanencia tranquila o sufrida.</p>
-                <div class="noticia-info-extra">
-                        <p class="noticia-autor">Marca.com</p>
-                        <p class="noticia-fecha">10 de abril de 2024</p>
-                </div>
-            </div>
+<div class="container">
+    <h1>Administración de Noticias</h1>
+    <input type="text" id="search" placeholder="Buscar noticias..." onkeyup="fetchData()" class="form-control mb-3">
 
-            <div class="noticia">
-                <h2><a href="https://www.vamosmisevillafc.com/pape-gueye-regresara-a-laliga/">Pape Gueye regresará a LaLiga</a></h2>
-                <img src="https://www.vamosmisevillafc.com/wp-content/uploads/2023/01/las-claves-para-el-fichaje-de-pa.jpg" alt="Imagen de la Noticia 2">
-                <p class="descripcion">Tras pasar los 4 meses de sanción por romper un contrato firmado, está cuajando una buena temporada en el Olympique de Marsella, con opciones de entrar en competición europea.</p>
-                <div class="noticia-info-extra">
-                        <p class="noticia-autor">Vamos Mi Sevilla</p>
-                        <p class="noticia-fecha">9 de abril de 2024</p>
-                </div>
-             </div>
-
-            <div class="noticia">
-                <h2><a href="https://www.vamosmisevillafc.com/pape-gueye-regresara-a-laliga/">Estos son los jugadores de LaLiga que acaban contrato este verano</a></h2>
-                <img src="https://estaticos-cdn.prensaiberica.es/clip/ae0cda80-7e86-452f-a1ba-323e83359c0d_16-9-discover-aspect-ratio_default_0.jpg">
-                <p class="descripcion">Muchos futbolistas quedarán libres a final de temporada y se convertirán en agentes libres para firmar por el equipo que deseen.</p>
-                <div class="noticia-info-extra">
-                        <p class="noticia-autor">Superdeporte</p>
-                        <p class="noticia-fecha">9 de abril de 2024</p>
-                </div>
-            </div>
-        </div>
+    <div id="noticias-list" class="row">
+        <!-- Las noticias se llenarán dinámicamente -->
     </div>
-</body>
-</html>
+
+    <div id="pagination-links" class="d-flex justify-content-center">
+        <!-- Los enlaces de paginación se cargarán aquí -->
+    </div>
+</div>
+
+<script>
+
+    document.addEventListener('DOMContentLoaded', function() {
+        fetchData(); // Carga inicial de datos
+    });
+
+    function fetchData(page = 1) {
+        var search = document.getElementById('search').value;
+        var url = `/adminnoticias?search=${search}&page=${page}`;
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            var noticiasList = document.getElementById('noticias-list');
+            noticiasList.innerHTML = '';
+            data.data.forEach(noticia => {
+                // Realizar una segunda llamada para obtener el nombre del equipo
+                fetch(`/adminnoticias/equipo/${noticia.equipo_id}`)
+                    .then(response => response.json())
+                    .then(equipo => {
+                        // Ahora tienes el nombre del equipo
+                        var noticiaHTML = `
+                            <div class="col-md-12 mb-4">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="Imagen de la noticia">
+                                    <div class="card-body">
+                                        <h3><a href="${noticia.link_de_la_web}">${noticia.titulo}</a></h2>
+                                        <p class="card-text">${noticia.fecha}</p>
+                                        <p class="card-text">${noticia.autor}</p>
+                                        <p class="card-text">${equipo.nombre}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+                        noticiasList.innerHTML += noticiaHTML;
+                    });
+            });
+
+            var paginationDiv = document.getElementById('pagination-links');
+            paginationDiv.innerHTML = data.links;
+        })
+        .catch(error => console.error('Error:', error));
+    }
+
+        // Esta función se encarga de manejar el evento de clic en los enlaces de paginación
+        function attachClickEventToPaginationLinks() {
+            document.querySelectorAll('#pagination-links a').forEach(item => {
+                item.addEventListener('click', function(e) {
+                    e.preventDefault(); // Evitar la navegación directa
+                    const page = this.getAttribute('href').split('page=')[1];
+                    fetchNoticias(page); // Llamar a la función para obtener las noticias de la página seleccionada
+                });
+            });
+        }
+
+        // Llamar a la función fetchNoticias al cargar la página para mostrar las primeras noticias
+        document.addEventListener('DOMContentLoaded', function() {
+            fetchNoticias();
+            attachClickEventToPaginationLinks();
+        });
+</script>
+
 @endsection
