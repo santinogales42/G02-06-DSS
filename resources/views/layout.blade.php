8dda976f6710b3f4c17fd0cab537411f90d969fd
Ruben <rcm144@alu.ua.es>
2024-05-15 10:21:15 +0200

    Mejora layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -98,27 +98,79 @@
         .navbar-brand {
             margin-left: 15px;
         }
-
-        .navbar .btn-outline-dark, .navbar .btn-light {
+        /*
+        .common-btn-style {
             margin-left: 15px;
-            background-color: #d9534f;
-        }
-
-        .dropdown-admin .dropdown-toggle {
-            margin-left: 15px;
-            border: 1px solid #6c757d;
-            color: #6c757d;
-            background-color: transparent;
+            border: 1.5px solid black;
+            color: black;
+            background-color: white;
             padding: 0.375rem 0.75rem;
             border-radius: 0.25rem;
+            display: inline-flex;
+            align-items: center;
+            height: 38px;
+
         }
 
-        .dropdown-admin .dropdown-toggle:hover,
-        .dropdown-admin .dropdown-toggle:focus {
-            color: #343a40;
-            background-color: #e9ecef;
+        .common-btn-style:hover,
+        .common-btn-style:focus {
+            color: red;
+            background-color: white;
             border-color: #6c757d;
         }
+        */
+
+
+        .common-btn-style {
+        font-size: 18px;
+        letter-spacing: 2px;
+        text-transform: uppercase;
+        display: inline-block;
+        text-align: center;
+        font-weight: bold;
+        padding: 0.7em 2em;
+        border: 3px solid Black;
+        border-radius: 2px;
+        position: relative;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.1);
+        color: black;
+        text-decoration: none;
+        transition: 0.3s ease all;
+        z-index: 1;
+        border-radius: 15px;
+        height: 60px;
+        }
+
+        .common-btn-style:before {
+        transition: 0.5s all ease;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        right: 50%;
+        bottom: 0;
+        opacity: 0;
+        content: '';
+        background-color: white;
+        z-index: -1;
+        }
+
+        .common-btn-style:hover, .common-btn-style:focus {
+        color: #ff3823;
+        
+        }
+
+        .common-btn-style:hover:before, .common-btn-style:focus:before {
+        transition: 0.5s all ease;
+        left: 0;
+        right: 0;
+        opacity: 1;
+        border-radius: 11px;
+        }
+
+        .common-btn-style:active {
+        transform: scale(0.9);
+        }
+
 
         .dropdown-menu {
             min-width: 10rem;
@@ -224,25 +276,25 @@
                 <img src="{{ asset('images/ligaicono.png') }}" alt="Logo La Liga" class="title-logo img-fluid" style="border: 2px solid red;">
             </a>
             <h1 class="roboto-flex-title flex-grow-1 text-center d-none d-md-block">LALIGA EA SPORTS 2023-24</h1>
-            <a href="{{ route('contacto') }}" class="btn btn-outline-dark ml-3">Contáctanos</a>
+            <a href="{{ route('contacto') }}" class="btn common-btn-style ml-3">Contáctanos</a>
             @if($isUserLoggedIn)
             <div class="navbar-text d-flex align-items-center dropdown-admin">
-                
-                    <button class="btn btn-outline-dark dropdown-toggle" id="userButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <div class="dropdown" id="userDropdown">
+                    <button class="btn common-btn-style dropdown-toggle" id="userButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span>{{ Session::get('userName') }}</span>
-                        <img src="{{ asset('images/usuario_r.png') }}" alt="Perfil" class="user-icon img-fluid">
+                        <img src="{{ asset('images/usuario_r.png') }} " alt="Perfil" class="user-icon img-fluid" style="margin-left: 5px; filter: invert(100%);">
                     </button>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userButton">
                         <a href="{{ route('perfilUsuario.index') }}" class="dropdown-item">Mi Perfil</a>
                         <a href="{{ route('favoritos.index') }}" class="dropdown-item">Favoritos</a>
                         <a href="{{ route('confirmar.cerrar.sesion') }}" class="dropdown-item" id="logoutButton">Cerrar sesión</a>
                     </div>
-                
+                </div>
             </div>
             @else
             <div class="navbar-text d-flex align-items-center">
-                <a class="btn btn-outline-dark ml-auto" href="{{ route('register') }}">Registrarse</a>
-                <a class="btn btn-dark ml-2" href="{{ route('login') }}">Iniciar Sesión</a>
+                <a class="btn common-btn-style ml-auto" href="{{ route('register') }}">Registrarse</a>
+                <a class="btn common-btn-style ml-2" href="{{ route('login') }}">Iniciar Sesión</a>
             </div>
             @endif
         </nav>
@@ -290,12 +342,6 @@
 </body>
 
 </html>
-
-
-
-
-
-
 
 
 
