77b503b7ac88928a3ca0a0c3f894683aeefde551
rld8 <domenec.raul@gmail.com>
2024-05-19 10:38:57 +0200

    arreglado create hilos

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -15,10 +15,7 @@
                     <label for="content" class="form-label">Contenido</label>
                     <textarea class="form-control" id="content" name="content" rows="5" required></textarea>
                 </div>
-                <div class="mb-3">
-                    <label for="image" class="form-label">Imagen (opcional)</label>
-                    <input type="file" class="form-control" id="image" name="image" accept="image/*">
-                </div>
+                
                 <button type="submit" class="btn btn-primary">Crear Hilo</button>
             </form>
         </div>
